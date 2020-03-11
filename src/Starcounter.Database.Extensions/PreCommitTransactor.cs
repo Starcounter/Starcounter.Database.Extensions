9e9b18046a8477c2050213ed09e5524b2aa4f36c
Per Samuelsson <per@starcounter.com>
2020-03-11 14:36:43 +0100

    First simple pre-commit support, using ITransactor decoration.

---
+++ src/Starcounter.Database.Extensions/PreCommitTransactor.cs
@@ -0,0 +1,40 @@
+using System;
+using Starcounter.Database.ChangeTracking;
+using System.Linq;
+using Microsoft.Extensions.Options;
+
+namespace Starcounter.Database.Extensions
+{
+    public class PreCommitTransactor : TransactorBase
+    {
+        class PreCommitContext : ContextBase
+        {
+            public PreCommitContext(IDatabaseContext context) : base(context) { }
+
+            public void ExecutePreCommitHooks(PreCommitOptions options)
+            {
+                foreach (var change in ChangeTracker.Changes.Where(c => c.ChangeType != ChangeType.Delete))
+                {
+                    var proxy = Get<object>(change.Id);
+                    var realType = proxy.GetType().BaseType;
+
+                    if (options.Delegates.TryGetValue(realType, out Action<IDatabaseContext, Change> action))
+                    {
+                        action(this, change);
+                    }
+                }
+            }
+        }
+
+        readonly PreCommitOptions hookOptions;
+
+        public PreCommitTransactor(ITransactor transactor, IOptions<PreCommitOptions> preCommitHookOptions)
+            : base(transactor)
+            => hookOptions = preCommitHookOptions.Value;
+
+        protected override IDatabaseContext EnterContext(IDatabaseContext db) => new PreCommitContext(db);
+
+        protected override void LeaveContext(IDatabaseContext db)
+            => ((PreCommitContext)db).ExecutePreCommitHooks(hookOptions);
+    }
+}
