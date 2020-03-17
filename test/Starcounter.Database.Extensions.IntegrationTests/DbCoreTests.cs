584b9b64017ca0b8349630fc0eee9c391c57658f
Per Samuelsson <per@starcounter.com>
2020-03-17 16:27:35 +0100

    A set of tests for the most inner database types, that are to be extended by the outer.

---
+++ test/Starcounter.Database.Extensions.IntegrationTests/DbCoreTests.cs
@@ -0,0 +1,69 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Starcounter.Database.ChangeTracking;
+using Xunit;
+
+namespace Starcounter.Database.Extensions.IntegrationTests
+{
+    public class DbCoreTests : ServicedTests
+    {
+        public class Person {}
+
+        [Fact]
+        public void ServiceSetupYieldExpectedImplementations()
+        {
+            var transactor = CreateServices().GetRequiredService<ITransactor>();
+            var context = transactor.Transact(db => db.GetType());
+
+            Assert.IsType<DbTransactor>(transactor);
+            Assert.IsType<DbContext>(context);
+        }
+
+        [Fact]
+        public void InsertOneReturnFirstId()
+        {
+            var t = CreateServices().GetRequiredService<ITransactor>();
+
+            t.Transact(db =>
+            {
+                var p = db.Insert<Person>();
+                Assert.Equal(1UL, db.GetOid(p));
+            });
+        }
+
+        [Fact]
+        public void InsertOneReturnExpectedChanges()
+        {
+            var t = CreateServices().GetRequiredService<ITransactor>();
+
+            t.Transact(db =>
+            {
+                var p = db.Insert<Person>();
+
+                var change = Assert.Single(db.ChangeTracker.Changes);
+                Assert.Equal(1UL, change.Oid);
+                Assert.Equal(ChangeType.Insert, change.Type);
+            });
+        }
+
+        [Fact]
+        public void DeleteResultInObjectBeingRemoved()
+        {
+            var t = CreateServices().GetRequiredService<ITransactor>();
+
+            var id = t.Transact(db =>
+            {
+                var p = db.Insert<Person>();
+                return db.GetOid(p);
+            });
+
+            t.Transact(db =>
+            {
+                var p = db.Get<Person>(id);
+                db.Delete(p);
+
+                Assert.Throws<ArgumentOutOfRangeException>(() => db.Get<Person>(id));
+            });
+        }
+    }
+}
