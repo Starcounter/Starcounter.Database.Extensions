3409b88e2c98c405ffc38d52bdefa60e9637d3e9
Per Samuelsson <per@starcounter.com>
2020-03-25 10:35:27 +0100

    New unit test ShouldRenderContextThatIndicatesNestingWhenRunningContinuation

--- test/Starcounter.Database.Extensions.IntegrationTests/NestedTransactorTests.cs
+++ test/Starcounter.Database.Extensions.IntegrationTests/NestedTransactorTests.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -31,6 +32,29 @@ namespace Starcounter.Database.Extensions.IntegrationTests
             });
 
             Assert.True(nested);
+        }
+
+        [Fact]
+        public void ShouldRenderContextThatIndicatesNestingWhenRunningContinuation()
+        {
+            var transactor = CreateServices(
+                s => s.Decorate<ITransactor, NestedTransactor>())
+                .GetRequiredService<ITransactor>();
+
+            // This execute as a top-level transaction, saving the context as
+            // a thread local.
+            var t = transactor.Transact(async db =>
+            {
+                // Yield to force continuation to be scheduled and
+                // hence assure we test actual asynchronous execution.
+                await Task.Yield();
+
+                // Inner transactor should find original context when
+                // back on original thread.
+                return transactor.Transact(db => db.IsNested());
+            });
+
+            Assert.True(t.GetAwaiter().GetResult());
         }
     }
 }
