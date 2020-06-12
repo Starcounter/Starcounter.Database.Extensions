ec2cd9a7528891b6ff3717abde0e9da61547f7a5
Konstantin <const.miyang@gmail.com>
2020-06-12 13:22:59 +0200

    Remove redundant PreCommitContext class

--- src/Starcounter.Database.Extensions/PreCommitTransactor.cs
+++ src/Starcounter.Database.Extensions/PreCommitTransactor.cs
@@ -7,38 +7,31 @@ namespace Starcounter.Database.Extensions
 {
     public class PreCommitTransactor : TransactorBase
     {
-        class PreCommitContext : ContextBase
-        {
-            public PreCommitContext(IDatabaseContext context) : base(context) { }
-
-            public void ExecutePreCommitHooks(PreCommitOptions options)
-            {
-                foreach (var change in ChangeTracker.Changes.Where(c => c.Type != ChangeType.Delete))
-                {
-                    var proxy = Get<object>(change.Oid);
-                    var realType = proxy.GetType().BaseType;
-
-                    if (options.Delegates.TryGetValue(realType, out Action<IDatabaseContext, Change> action))
-                    {
-                        action(this, change);
-                    }
-                }
-            }
-        }
-
         readonly PreCommitOptions hookOptions;
 
         public PreCommitTransactor(ITransactor transactor, IOptions<PreCommitOptions> preCommitHookOptions)
             : base(transactor)
             => hookOptions = preCommitHookOptions.Value;
 
-        protected override IDatabaseContext EnterContext(IDatabaseContext db) => new PreCommitContext(db);
-
         protected override void LeaveContext(IDatabaseContext db, bool exceptionThrown)
         {
             if (!exceptionThrown)
             {
-                ((PreCommitContext)db).ExecutePreCommitHooks(hookOptions);
+                ExecutePreCommitHooks(db, hookOptions);
+            }
+        }
+
+        protected virtual void ExecutePreCommitHooks(IDatabaseContext db, PreCommitOptions options)
+        {
+            foreach (var change in db.ChangeTracker.Changes.Where(c => c.Type != ChangeType.Delete))
+            {
+                var proxy = db.Get<object>(change.Oid);
+                var realType = proxy.GetType().BaseType;
+
+                if (options.Delegates.TryGetValue(realType, out Action<IDatabaseContext, Change> action))
+                {
+                    action(db, change);
+                }
             }
         }
     }
